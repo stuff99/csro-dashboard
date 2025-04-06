12ba99b34edae38f03456e6d043829319c62f408
Bhavesh2k4 <Bhavesh.oct2k4@gmail.com>
2025-04-06 13:01:19 +0530

    user ID can be extracted using token

--- auth.ts
+++ auth.ts
@@ -2,8 +2,35 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
 import authConfig from "./auth.config"
+import { getUserById } from "./data/user"
+import { UserRole } from "@prisma/client"
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+      async session({session, token}){
+          if(token.sub && session.user){
+              session.user.id = token.sub
+          }
+
+          if(token.role&&session.user){
+              session.user.role=token.role as UserRole
+          }
+          return session
+
+      },
+      //token has name,email,picture,sub (your id),iat , exp,jti
+      async jwt({token}){
+          if(!token.sub){
+              return token
+          }
+          const existingUser=await getUserById(token.sub)
+          if(!existingUser){
+              return token
+          }
+          token.role=existingUser.role
+          return token
+      }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
