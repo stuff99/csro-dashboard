3849f53cd03ddee81f64675ce9b8ee079140c791
Josh Davis <josh.davis@prutech.com>
2025-06-03 22:11:36 -0600

    password fix

--- app/(auth)/new-password/page.tsx
+++ app/(auth)/new-password/page.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from "react"
 import NewPasswordForm from "@/components/auth/new-password-form"
 
 const NewPasswordPage = () => {
     return (
-        <NewPasswordForm/>
+        <Suspense fallback={<div>Loading...</div>}>
+            <NewPasswordForm/>
+        </Suspense>
     )
 }
 
